0650b4fd7974ff2a649105868ff04d814f6a5cd9
Esa Lakaniemi <esalaka@gmail.com>
2013-01-13 03:23:11 +0200

    Perkele

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ CFLAGS=-ggdb -O2 -Wall -Wextra -Werror -pedantic -pedantic-errors
 CPPFLAGS=
 LDFLAGS=
 
-cblia: main.o
+cblia: main.o parser.o
 	gcc $(CFLAGS) -o cblia main.o $(LDFLAGS)
 
 clean:
--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 
-#define LERR_NO_INPUT 1
+#include "parser.h"
 
-int lill_parser(FILE *);
+#define LERR_NO_INPUT 1
 
 int main(int argc, char *argv[])
 {
@@ -35,53 +35,3 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
-int lill_parser(FILE *input)
-{
-	struct lill_def {
-		char *whitespace; /* Null-terminated!!! */
-	} def;
-
-	struct lill_state {
-		int cur;
-		char token[128];
-	} state;
-
-	struct lill_parser_token {
-		/* DATA */
-		struct lill_parser_token *next;
-	};
-
-	/* We only reserve space for 200 tokens */
-	/* OTOH this program is basically a hack, it should suffice */
-	struct lill_token_pool {
-		char pool[200*sizeof(struct lill_parser_token)], *pool_pointer;
-	} pool;
-	
-	/* ASCII whitespace only */
-	/* Gomen. I'm going the "ASCII or a derivative" route */
-	def.whitespace = " \t";
-
-	state.cur = 0;
-	memset(state.token, '\0', sizeof(state.token));
-
-	memset(pool.pool, 0, sizeof(pool.pool));
-	pool.pool_pointer = pool.pool; /* I'm serious */
-
-	/* PARSE */
-
-	/* Because CB, I'll clear state on each newline */
-	/*
-	 * This means that before ) or after As \r\n is
-	 * always unexpected!
-	 */
-
-	state.cur = fgetc(input);
-
-	/* Skip beginning whitespace */
-	while (state.cur && strchr(def.whitespace, state.cur))
-		state.cur = fgetc(input);
-
-	if 
-	
-	return 0;
-}
