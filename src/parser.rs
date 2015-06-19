a455182d271ccb6d7d6fefb69bcf24e706ba2a9a
Esa Lakaniemi <esalaka@gmail.com>
2015-06-19 03:49:09 +0300

    How'd I forget the actual module

---
+++ src/parser.rs
@@ -0,0 +1,12 @@
+use tokenizer;
+
+
+pub enum Node {
+    Empty
+}
+
+pub fn parse(it: &tokenizer::TokenIterator) -> Node {
+    // TODO: Make a stack-based parser thingy!
+    // (Or recursive descent? IDK let's look at it)
+    unimplemented!();
+}
