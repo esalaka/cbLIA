c054ba4732c3b3a93bbef944ed0c39b482c517b0
Esa Lakaniemi <esalaka@gmail.com>
2015-07-01 04:20:03 +0300

    Those don't need to be there

--- old_c/util.h
+++
@@ -1,8 +0,0 @@
-#ifndef CBLIA_UTIL_H
-#define CBLIA_UTIL_H
-
-int is_varname_latin(unsigned char c);
-int is_varname_letter(unsigned char c);
-int is_varname_legal(unsigned char c);
-
-#endif /* CBLIA_UTIL_H */
